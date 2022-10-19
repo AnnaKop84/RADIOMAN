2f74b55e1e2fcddcd6ca17016bf11dd737c7d269
Anna Kopeikina <karavay3@gmail.com>
2022-10-19 21:33:09 +0300

    added Radioman

---
+++ src/main/java/ru/netology/radio/Radio.java
@@ -0,0 +1,14 @@
+package ru.netology.radio;
+public class Radio {
+    public int currentStation;
+    public int firstStation =0;
+    public int lastStation = 9;
+    public int currentVolume;
+    public int lowVolume=0;
+    public int highVolume = 10;
+    public int getCurrentStation(){
+        return currentStation;
+    }
+
+
+}
